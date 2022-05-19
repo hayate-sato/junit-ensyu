2965698648b529e9e93d63399db4d79e9e58e61e
hayate-sato <hayate.sato@rakus-partners.co.jp>
2022-05-19 13:07:33 +0900

    1-4終了

---
+++ src/test/java/junit/tutorial/ex01/e04/NumberUtilsTest.java
@@ -0,0 +1,43 @@
+package junit.tutorial.ex01.e04;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NumberUtilsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		boolean answer = NumberUtils.isEven(0);
+		assertEquals(true, answer);
+		
+	}
+
+	@Test
+	void test2() {
+		boolean answer = NumberUtils.isEven(1);
+		assertEquals(false, answer);
+		
+	}
+
+}
